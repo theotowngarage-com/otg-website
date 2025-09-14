7ee2fe6298075ea7b63558b431f9596b9422f177
amrsoll <axel+github@soll.xyz>
2025-09-15 01:26:48 +0200

    cleanup

--- backend/main.go
+++ backend/main.go
@@ -8,16 +8,12 @@ import (
 
 	"github.com/stripe/stripe-go/v82"
 	"github.com/stripe/stripe-go/v82/checkout/session"
-	"github.com/stripe/stripe-go/v82/price"
-	"github.com/stripe/stripe-go/v82/product"
 	"golang.org/x/crypto/bcrypt"
 	gomail "gopkg.in/mail.v2"
 )
 
 func main() {
-	// This test secret API key is a placeholder. Don't include personal details in requests with this key.
-	// To see your test secret API key embedded in code samples, sign in to your Stripe account.
-	// You can also find your test secret API key at https://dashboard.stripe.com/test/apikeys.
+	// You can find your test secret API key at https://dashboard.stripe.com/test/apikeys.
 	stripe.Key = "sk_test_xxxx...xxxxxx"
 
 	http.Handle("/", http.FileServer(http.Dir("public")))
@@ -36,7 +32,6 @@ Stripe enables cards and other common payment methods for you by default,
 and you can enable or disable payment methods directly in the Stripe Dashboard.
 */
 func createCheckoutSession(w http.ResponseWriter, request *http.Request) {
-	// log.Print(request)
 	log.Print("received something")
 	if request.ParseForm() != nil || !validateInput(request.Form) {
 		log.Fatal("malformed request") // highlight - potential attack
@@ -45,7 +40,6 @@ func createCheckoutSession(w http.ResponseWriter, request *http.Request) {
 		return
 	}
 	log.Print("form parsed and validated")
-	// form := &request.Form
 	for key, value := range request.Form {
 		log.Print("%s : %s", key, value)
 	}
@@ -107,27 +101,6 @@ func successCheckoutSession(w http.ResponseWriter, request *http.Request) {
 	// Redirect to the main site
 	http.Redirect(w, request, "http://localhost:1313/checkout/success/", http.StatusSeeOther)
 	// TODO: Send confirmation mail to the user ?
-}
-
-func create_subscription() {
-	product_params := &stripe.ProductParams{
-		Name:        stripe.String("Starter Subscription"),
-		Description: stripe.String("$12/Month subscription"),
-	}
-	starter_product, _ := product.New(product_params)
-
-	price_params := &stripe.PriceParams{
-		Currency: stripe.String(string(stripe.CurrencyUSD)),
-		Product:  stripe.String(starter_product.ID),
-		Recurring: &stripe.PriceRecurringParams{
-			Interval: stripe.String(string(stripe.PriceRecurringIntervalMonth)),
-		},
-		UnitAmount: stripe.Int64(1200),
-	}
-	starter_price, _ := price.New(price_params)
-
-	fmt.Println("Success! Here is your starter subscription product id: " + starter_product.ID)
-	fmt.Println("Success! Here is your starter subscription price id: " + starter_price.ID)
 }
 
 func send_mail(to string, subject string, body string) {
