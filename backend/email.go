a46a91754989e2b4e9607d837097bc367c84f999
amrsoll <axel+github@soll.xyz>
2025-09-16 18:15:37 +0200

    Add welcome email functionality

---
+++ backend/email.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+
+	gomail "gopkg.in/mail.v2"
+)
+
+type Template struct {
+	path    string
+	subject string
+}
+
+var Welcome Template = Template{
+	path:    "email_templates/welcome.html",
+	subject: "Welcome to The O'Town Garage!",
+}
+
+func sendMail(to string, user User, email_template Template) {
+	// Parse the template
+	tmpl, err := template.ParseFiles(email_template.path)
+	if err != nil {
+		fmt.Println("Error parsing template:", err)
+		return
+	}
+
+	// Execute the template with dynamic data
+	var body bytes.Buffer
+	err = tmpl.Execute(&body, struct {
+		Name string
+		Link string
+	}{Name: user.name, Link: "https://discord.gg/CGBgKNwT"})
+
+	if err != nil {
+		fmt.Println("error executing template:", err)
+		return
+	}
+
+	// Create a new message
+	message := gomail.NewMessage()
+	message.SetHeader("From", "info@theotowngarage.com")
+	message.SetHeader("To", to)
+	message.SetHeader("Subject", email_template.subject)
+	message.SetBody("text/html", body.String())
+
+	dialer := gomail.NewDialer("smtppro.zoho.com", 465, "info@theotowngarage.com", "<password>")
+	if err := dialer.DialAndSend(message); err != nil {
+		fmt.Println("Error:", err)
+		panic(err)
+	} else {
+		fmt.Println("Email sent successfully!")
+	}
+}
