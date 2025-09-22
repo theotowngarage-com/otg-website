ea3171da6805a25786a31d8597d360bf1f7a5673
amrsoll <axel+github@soll.xyz>
2025-09-22 09:09:23 +0200

    Users who canceled may now re-subscribe

--- backend/user.go
+++ backend/user.go
@@ -1,19 +1,47 @@
 package main
 
-import "github.com/gorilla/sessions"
+import (
+	"net/http"
+
+	"github.com/gorilla/sessions"
+	"github.com/stripe/stripe-go/v82"
+)
 
 type User struct {
-	Email       string
-	Name        string
-	Phone       string
-	Password    []byte
-	Active      bool
-	Customer_id string
+	Email         string
+	Name          string
+	Phone         string
+	Password      []byte
+	PlainPassword string
+	Active        bool
+	CustomerID    string
 }
 
 func toUser(session sessions.Session) User {
 	return User{
-		Email:       session.Values["email"].(string),
-		Customer_id: session.Values["customer_id"].(string),
+		Email:      session.Values["email"].(string),
+		CustomerID: session.Values["customer_id"].(string),
+	}
+}
+
+func FormToUser(request *http.Request) User {
+	return User{
+		Name:          request.Form.Get("name"),
+		Email:         request.Form.Get("email"),
+		PlainPassword: request.Form.Get("pass"),
+		Phone:         request.Form.Get("phone"),
+	}
+}
+
+func (user *User) AddToStripeMetadata(params *stripe.CheckoutSessionParams) {
+	params.AddMetadata("email", user.Email)
+	if len(user.Name) > 0 {
+		params.AddMetadata("name", user.Name)
+	}
+	if len(user.Phone) > 0 {
+		params.AddMetadata("phone", user.Name)
+	}
+	if len(user.PlainPassword) > 0 {
+		params.AddMetadata("pass", user.Name)
 	}
 }
